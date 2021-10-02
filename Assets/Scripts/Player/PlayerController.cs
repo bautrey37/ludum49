5b1785a32d3a14f98a704a08c713104389e17aad
Anton Perepelenko <anton.perepelenko@cgi.com>
2021-10-02 15:37:59 +0300

    * Increased map size * Camera sticks to the player * Added one-sided road to the map

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
         hud = FindObjectOfType<HUD>();
         rb = GetComponent<Rigidbody2D>();
         currentControls = Controls.Normal;
-        InvokeRepeating("ChangeControls", 5, 5); // Call the switch controls function every 5 seconds
+        InvokeRepeating("ChangeControls", 5, 10); // Call the switch controls function every 5 seconds
     }
 
 	void Update()
