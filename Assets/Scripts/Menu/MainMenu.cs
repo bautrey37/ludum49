8e2ea669efade02ec42126588b2c371b956548f1
bautrey37 <brandon.c.autrey@gmail.com>
2021-10-02 14:50:55 +0300

    Added volume slider
    This volume slider saves the value to user prefs. Which upon reload of
    the game will have the same value.

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider Volume;
+
+    // private void Awake()
+    // {
+    //     Volume = gameObject.GetComponent<Slider>();
+    // }
+
+    private void Start()
+    {
+        Volume.value = GameSettings.Instance.Volume;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("BrandonScene");
+    }
+
+    public void AdjustVolume()
+    {
+
     }
 
     public void QuitGame()
